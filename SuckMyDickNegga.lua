7667ea17dfa99bb7de82d8ba009d87d8ad268847
WhoAsked9 <134094752+WhoAsked9@users.noreply.github.com>
2023-10-17 20:00:11 +0500

    Create SuckMyDickNegga.lua

---
+++ SuckMyDickNegga.lua
@@ -0,0 +1,12 @@
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.9.16) ~  Much Love, Ferib 
+
+]]--
+
+local v0=loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))();local v1=v0:MakeWindow({Name="SHITTING ECONOMY",HidePremium=false,SaveConfig=true,ConfigFolder="OrionTest"});local v2=v1:MakeTab({Name="CAMAVINGAAA",Icon="rbxassetid://4483345998",PremiumOnly=false});v2:AddButton({Name="Press Me if u want get ultamite pogo stick (req. 1 coins nd rj )",Callback=function()local v3={[1 + 0 ]="ultamite",[933 -(857 + 74) ]=569 -(367 + 201) ,[7 -4 ]="special"};game:GetService("ReplicatedStorage"):WaitForChild("stickBought"):FireServer(unpack(v3));end});v0:Init();
